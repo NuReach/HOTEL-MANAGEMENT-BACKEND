c8ebad60c43ae6285e3dad25d93e70ec47374397
NuReach <nureach46@gmail.com>
2024-06-05 00:15:03 +0700

    update room

--- app/Http/Controllers/Api/RoomTypeController.php
+++ app/Http/Controllers/Api/RoomTypeController.php
@@ -52,7 +52,7 @@ class RoomTypeController extends Controller
             return response()->json(['message'=>'Something went wrong with creating room type!!'], 500);
         }
 
-        $imageUrl = null;
+        $imageUrl = 'default.png';
 
         if ( $request->hasFile('image') ) {
 
@@ -105,12 +105,66 @@ class RoomTypeController extends Controller
     {
         $roomType = RoomType::findOrFail($id);
 
+        $room = $roomType->room;
+
         $request->validate([
             'name' => 'required|string',
             'user_id' => 'required|integer|string',
+            'total_adult' => 'nullable|string',
+            'total_child' => 'nullable|string',
+            'room_capacity' => 'nullable|string',
+            'price' => 'required|string', 
+            'size' => 'nullable|string',
+            'view' => 'nullable|string',
+            'bed_style' => 'nullable|string',
+            'discount' => 'nullable|integer',
+            'short_desc' => 'nullable|string',
+            'description' => 'nullable|string',
+            'status' => 'nullable|integer|in:0,1',
         ]);
 
-        $roomType->update($request->all());
+        $roomType->name = $request->name;
+        $roomType->user_id = $request->user_id;
+
+        $room->total_adult = $request->total_adult;
+        $room->total_child = $request->total_child;
+        $room->room_capacity = $request->room_capacity;
+        $room->price = $request->price;
+        $room->size = $request->size;
+        $room->view = $request->view;
+        $room->bed_style = $request->bed_style;
+        $room->discount = $request->discount;
+        $room->short_desc = $request->short_desc;
+        $room->description = $request->description;
+        $room->status = $request->status;
+       
+
+        if ($request->hasFile('image')) {
+            //add image to folder
+            $user = auth()->user(); 
+            $image = $request->file('image');
+            $imageName = $user->name. 'room' . time() . '.' . $image->getClientOriginalExtension();
+            $image->move(public_path('images'), $imageName);
+            $imageUrl = asset('images/' . $imageName);
+            //update image url
+            $room->image = $imageUrl;
+            //delete previous image
+            $oldImageUrl = $room->image;
+            if ($oldImageUrl) {
+                $filename = basename($oldImageUrl);
+                $image_path = public_path('images/' . $filename);
+                if (file_exists($image_path)) {
+                    unlink($image_path);
+                }
+            }
+
+        }else{
+            $room->image = $request->image;
+        }
+
+        $roomType->save();
+        $room->save();
+
 
         return response()->json([
             'roomtype'=>$roomType,
@@ -123,10 +177,12 @@ class RoomTypeController extends Controller
      */
     public function destroy($id)
     {
-        $roomType = RoomType::findOrFail($id)->delete();
-
-        $rooms =  Room::where('roomtype_id', $roomType->id)->delete();
-
-        return response()->json(['message' => 'Room type deleted successfully'], 200);
+        $roomType = RoomType::findOrFail($id);
+    
+        Room::where('roomtype_id', $roomType->id)->delete();
+    
+        $roomType->delete();
+    
+        return response()->json(['message' => 'Room type and associated rooms deleted successfully'], 200); 
     }
 }
