731c8a27f636775accdf806e8bfe2a50fc0272d5
NuReach <nureach46@gmail.com>
2024-06-07 22:17:52 +0700

    update delete image

--- app/Http/Controllers/Api/MultiImageController.php
+++ app/Http/Controllers/Api/MultiImageController.php
@@ -11,6 +11,11 @@ class MultiImageController extends Controller
     public function deleteImageGallary(  $gallaryId ) {
 
         $image = MultiImage::findOrFail($gallaryId);
+        $filename = basename($image);
+        $image_path = public_path('images/' . $filename);
+        if (file_exists($image_path)) {
+            unlink($image_path);
+        }
         $image->delete();
         return response()->json(['message' => 'Gallary is deleted '], 200);    
         
